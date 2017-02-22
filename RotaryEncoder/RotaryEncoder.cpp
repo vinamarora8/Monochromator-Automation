c9bf4242a04a24c293691f86b8d0ad3f9a16bf05
vinamarora8 <vinam.arora8@gmail.com>
2017-02-22 22:33:42 +0530

    Changes Rotary Encoder's Gray->Dec Algo. It works fine now

--- RotaryEncoder/RotaryEncoder.cpp
+++ RotaryEncoder/RotaryEncoder.cpp
@@ -21,17 +21,30 @@ void RotaryEncoder::grayToDecimal(int &gray_val, int bl) {
 	grayToDecimal(gray_val, bl-1);
 }
 
+int RotaryEncoder::grayToBin(int grayVal, int nbits ) {
+  // Bn-1 = Bn XOR Gn-1   source of method:  http://stackoverflow.com/questions/5131476/gray-code-to-binary-conversion but include correction 
+    int binVal = 0;
+  	bitWrite(binVal, nbits - 1, bitRead(grayVal, nbits - 1)); // MSB stays the same
+	for (int b = nbits - 1; b > 0; b-- ) {
+		// XOR bits
+		if (bitRead(binVal, b) == bitRead(grayVal, b - 1)) { // binary bit and gray bit-1 the same
+			bitWrite(binVal, b - 1, 0);
+		}
+		else {
+   			bitWrite(binVal, b - 1, 1);
+		}
+	}
+	return binVal;
+}
+
 int RotaryEncoder::getPosition(const int *pins) {
 	// Declare a variable which will be the answer
 	int pos = 0;
 	
 	// Write bits into it - in gray code though
 	for (int i=0; i<8; i++) {
-		Serial.print(digitalRead(pins[i]));
-		bitWrite(pos, i, digitalRead(pins[i]));
+		bitWrite(pos, i, !digitalRead(pins[7-i]));
 	}
-	Serial.println();
 	// Convert the Gray value of position to Decimal
-	grayToDecimal(pos);
-	return pos;
+	return grayToBin(pos, 8);
 }
--- RotaryEncoder/RotaryEncoder.h
+++ RotaryEncoder/RotaryEncoder.h
@@ -8,6 +8,7 @@ public:
 	RotaryEncoder() {};			// Constructor
 	int invert(int);			// For Gray->Dec conversion
 	void grayToDecimal(int &, int bl = 8);	// Gray->Decimal
+	int grayToBin(int, int);
 	int getPosition(const int *);		// Finally used function
 };
 
