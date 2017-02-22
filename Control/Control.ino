58c504b66ec89a837f156891169463aee63a67ec
vinamarora8 <vinam.arora8@gmail.com>
2017-02-22 22:35:47 +0530

    Adds status md prints to Control.ino

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,6 +1,5 @@
 /* TODO
  *  - Check Resolution values
- *  - Check stepper polarity
  */
 
 #include <RotaryEncoder.h>
@@ -17,7 +16,7 @@ int current_wavelength = 300;
 void setup() {
   // Initialize Serial monitor
   Serial.begin(9600);
-  
+  Serial.println("Setting pins for Rotary Encoder");
   // Set up Rotary Encoder Pins
   for (int i=0; i<8; i++) {
     pinMode(RotEnc[i], INPUT);
@@ -26,7 +25,7 @@ void setup() {
 
   // Set up stepper motor
   // 
-  // 
+  stepctrl.set_step_delay(100);
   //
 
   // Initialize the motor
@@ -38,12 +37,12 @@ void loop() {
   // main code
   // Read serial input
   if(Serial.available() > 0) {
-    int inp = Serial.read();
+    int inp = Serial.parseInt();
 
     // Check if this is a wavelength, a command or a trash number
     if (inp <= 700 && inp >= 300)
       RTControl(inp);
-    else if (inp = 701)
+    else if (inp == 701)
       initialize();
     else
       Serial.println(0);
@@ -52,21 +51,34 @@ void loop() {
 }
 
 void initialize() {
+  Serial.println("Initializing");
   // For testing purposes:
-  EEPROM.write(0, 0);
-
+  EEPROM.write(0, 10);
+  
   // Read initial rotary encoder value (stored in EEPROM address 0)
   int init_val = EEPROM.read(0);
-
+  
+  Serial.print("init-val = ");
+  Serial.println(init_val);
+  
   // Read current rotary encoder value
   int current_val = rotlib.getPosition(RotEnc);
-
+  
+  Serial.print("Shaft currently at: ");
+  Serial.println(current_val);
+  
   // Goto Init Val
   //
   // Calculate number of steps it takes to reach init val
   // Resolutions: Stepper motor - 200 steps per cycle
   //              Rotary Encoder- 256 steps per cycle
-  int steps = (init_val - current_val)*(200/256);
+  int steps = (init_val-current_val);
+  steps *= 200;
+  steps /= 256;
+
+  Serial.print("Steps required: ");
+  Serial.println(steps);
+  
   //
   // Travel that many steps
   stepctrl.step(steps);
@@ -74,8 +86,8 @@ void initialize() {
   // Veryify
   // If not reached, recall this function
   current_val = rotlib.getPosition(RotEnc);
-  if (init_val != current_val)
-    initialize();
+//  if (init_val != current_val)
+ //   initialize();
 
   // Testing helper
   Serial.println(1);
@@ -84,14 +96,23 @@ void initialize() {
 void RTControl(int wavelength) {
   // Calculate number of steps to move
   // Resolution of Stepper motor : 200 steps per cycle
-  int steps = (wavelength - current_wavelength)*(200/700);
+  int steps = (wavelength - current_wavelength);
+  steps *= 2;
+  steps /= 7;
 
+  Serial.print("Current wavelength: ");
+  Serial.println(current_wavelength);
   // Travel that many steps
   stepctrl.step(steps);
 
+  Serial.print("Steps required: ");
+  Serial.println(steps);
   // Update current wavelength
   current_wavelength = wavelength;
-  
+
+  Serial.print("New wavelength: ");
+  Serial.println(current_wavelength);
   // Testing helper
   Serial.println(1);
+  Serial.println();
 }
