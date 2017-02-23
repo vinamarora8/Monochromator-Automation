a9fa8be3d4f9146c7826250a26a805ac3cea95e4
vinamarora8 <vinam.arora8@gmail.com>
2017-02-23 08:02:51 +0530

    Adds Callibrate.ino - Untested

---
+++ Callibrate/Callibrate.ino
@@ -0,0 +1,51 @@
+#include <StepperControl.h>
+#include <RotaryEncoder.h>
+#include <EPROM.h>
+
+// Pins for Rotary Encoder and Stepper Motor
+const int RotEnc[] = {2,3,4,5,6,7,8,9};
+const int StepMot[] = {10,11,12,13};
+
+// Libraries for operating the above
+RotaryEncoder rotlib;
+StepperControl stepctrl(StepMot);
+
+void setup() {
+	// Initialize Serial Monitor
+	Serial.begin(9600);
+	
+	// Set up Rotary Encoder Pins
+	for (int i=0; i<8; i++) {
+		pinMode(RotEnc[i], INPUT);
+		digitalWrite(RotEnc[i], HIGH);
+	}
+	
+	// Stepper Motor already set up by the library
+	stepctrl.set_step_delay(100);
+
+	// Print "READY"
+	Serial.println("READY");
+}
+
+void loop() {
+	// Main Code
+	// Read Serial Input
+	if (Serial.available() > 0) {
+		int steps = Serial.parseInt();
+
+		// If steps == 0, save the RotEnc state
+		// Write only if the state is different than the already stored one
+		// to increase EEPROM life
+		if (steps == 0){
+			int current_val = rotlib.getPosition(RotEnc);
+			if (current_val != EEPROM.read(0))
+				EEPROM.write(0, current_val);
+			Serial.println("DONE");
+		}
+
+		// Otherwise take as many steps as the input
+		else {
+			stepctrl.step(steps);
+		}
+	}
+}
