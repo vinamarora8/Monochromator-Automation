d123629626f35875296684554b9504be44332ab9
vinamarora8 <vinam.arora8@gmail.com>
2017-05-10 03:20:14 +0530

    Fixes bugs, syntax errors of Control.py

--- Control/Control.py
+++ Control/Control.py
@@ -5,13 +5,14 @@ import sys
 from os import system
 
 # make sure the user enters arduino's location as a cl argument
-if (sys.argv != 2):
-	print("usage: " + sys.argv[0] + " <location of arduino>")
+if (len(sys.argv) != 2):
+	sys.stderr.write("usage: " + sys.argv[0] + " <location of arduino>")
+	exit()
 
 _arduino_location = sys.argv[1]
 
 # TODO: test this past: Compile and Upload to Arduino
-system("arduino --upload Control.ino --port " + _arduino_loaction)
+system("arduino --upload Control.ino --port " + _arduino_location)
 
 # Connect to Arduino using Serial
 print "Connecting to Arduino... "
@@ -31,19 +32,23 @@ print "[3] Exit Control\t\t\t: exit"
 
 repeat = True
 
+sys.stdout.write("\n")
 while (repeat):
-	a = raw_input("Enter command: ")
-	
+	a = raw_input(">> Enter command: ")
+
 	if (a == 'init'):
 		_arduino.write('701')
-
+		print "Initializing..."			
 	elif (a == 'exit'):
 		repeat = False
-
+		print "Exiting... "	
 	else:
 		try:
 			a = int(a)
-			if (a <= 700 && a >= 300):
+			if (a <= 700 and a >= 300):
 				_arduino.write(str(a))
+				print "Going to : " + str(a) + "nm"
+			else:
+				print "Unsupported wavelength"
 		except ValueError:
 			print "Invalid command"
